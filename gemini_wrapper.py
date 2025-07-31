6707ae3ee01467390a759930b941b79b6adb812b
Shraddha <161319636+mkmpok@users.noreply.github.com>
2025-07-31 21:43:49 +0530

    Update gemini_wrapper.py

--- gemini_wrapper.py
+++ gemini_wrapper.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("AIzaSyCH8TJUfsuGqDOzPg1tlpVNfGaDY5f8dWQ"))
+genai.configure(api_key=os.getenv("YOUR_GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def get_response(prompt):
